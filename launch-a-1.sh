6852ffccd3897844f1b817b15d29913ac96fa33b
discoposse <eric@discoposse.com>
2019-11-16 12:38:31 -0500

    updating launch script

--- launch-a-1.sh
+++ launch-a-1.sh
@@ -3,10 +3,7 @@
 # Start all the things 
 echo Starting Nomad using Server + Client file
 cd ~
-cp /vagrant/nomad-server-a-1.hcl /etc/nomad.d/nomad-server-a-1.hcl
+cp /vagrant/server-east.hcl /etc/nomad.d/nomad-server-a-1.hcl
 sudo nohup nomad agent -config /etc/nomad.d/nomad-server-a-1.hcl &>nomad.log  &
 
-echo Joining cluster
-cd ~
-cp /vagrant/nomad-client-a-1.hcl /etc/nomad.d/nomad-client-a-1.hcl
-sudo nohup nomad agent -config /etc/nomad.d/nomad-client-a-1.hcl &>nomad.log  &
+
